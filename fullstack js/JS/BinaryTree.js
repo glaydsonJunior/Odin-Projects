9e169103b6b165f976714965ec791dd057c79d53
Glaydson <glaydsoncomp15@gmail.com>
2025-04-09 15:41:54 +0100

    Remove function BST

--- fullstack js/JS/BinaryTree.js
+++ fullstack js/JS/BinaryTree.js
@@ -14,7 +14,6 @@ function Tree(){
         }
     
         let currentNode = this.root;
-    
         while (true) {
             if (value < currentNode.value) {
                 if (currentNode.left == null) {
@@ -57,23 +56,89 @@ function Tree(){
         }
         return null;
     }
+
+    this.removeFromNode = (temp, v) => {
+        if(temp.right != null && temp.right.value == v){
+            if(temp.right.right == null && temp.right.left == null){
+                temp.right = null
+            }else if(temp.right.right == null && temp.right.left != null){
+                temp.right = temp.right.left
+            }else if(temp.right.right != null && temp.right.left == null){
+                temp.right = temp.right.right
+            } else {
+                if(temp.value < temp.right.right.value){
+                    const leftTemp = temp.right.left
+                    temp.right = temp.right.right
+                    temp.right.left = leftTemp
+                } else if(temp.value <= temp.right.left) {
+                    const rightTemp = temp.right.right
+                    temp.right = temp.right.left
+                    temp.right.right = rightTemp
+                }
+            }
+            return true
+        }
+        if(temp.left != null && temp.left.value == v){
+                if(temp.left.right == null && temp.left.left == null){
+                    temp.left = null
+                }else if(temp.left.right == null && temp.left.left != null){
+                    temp.left = temp.left.left
+                }else if(temp.left.right != null && temp.left.left == null){
+                    temp.left = temp.left.right
+                } else {
+                    if(temp.value < temp.left.right.value){
+                        const leftTemp = temp.left.left
+                        temp.left = temp.left.right
+                        temp.left.left = leftTemp
+                    } else if(temp.value <= temp.right.left) {
+                        const rightTemp = temp.left.right
+                        temp.left = temp.left.left
+                        temp.left.right = rightTemp
+                    }
+                }
+            return true
+        }
+        return false
+    }
+
+
+    this.remove = function(v){
+        let temp = this.root
+        while((temp.right != null || temp.left != null)){
+            if(this.removeFromNode(temp, v))break;
+            if(temp.value > v)temp = temp.left
+            if(temp.value < v)temp = temp.right
+        }
+        return temp
+    }
+
+
+    this.dfs = (n) => {
+        if(n == null)return;
+        console.log(n.value)
+        this.dfs(n.left)
+        this.dfs(n.right)
+    }
     
 }
 
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
+function prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) return;
 
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+}
     let a = new Tree()
     a.buildTree([10, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-    console.log(a.find(67))
+    a.remove(9)
+    a.remove(9)
+    a.remove(67)
+    prettyPrint(a.root)
